7779181bae85231d6ecb51bc2db2d9b6841bb9f4
sweathasrir25csc-commits <sweathasrir25csc@vetias.ac.in>
2026-06-12 12:29:56 +0530

    day7

--- src/day3.java
+++ src/day3.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
-public class day3 {
-}
+ void main() {
+         int[] marks={87,32,44,89,23};
+         System.out.println("Pass mark");
+         for(int mark:marks){
+             if(mark>=40){
+                 System.out.println(mark+"");
+             }
+         }
+         System.out.print("fail mark");
+         for(int mark:marks){
+             if(mark<40) {
+                 System.out.print(mark +"");
+             }
+        }
+    }
---
+++ src/day7.java
@@ -0,0 +1,51 @@
+public class day7{
+    static class Request {
+        String name;
+        String date;
+        String reason;
+        Request(String name, String date, String reason){
+            this.name = name;
+            this.date = date;
+            this.reason = reason;
+        }
+        void approve() {
+            System.out.println("Approving request");
+        }
+        void reject() {
+            System.out.println("Reject request");
+        }
+    }
+    static class LeaveRequest extends Request {
+
+        LeaveRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+        void approveLeaveRequest() {
+            System.out.println("Approving leave request");
+        }
+        void rejectLeaveRequest() {
+            System.out.println("Reject leave request");
+        }
+    }
+    static class OnDutyRequest extends Request {
+
+        OnDutyRequest(String name, String date, String reason) {
+            super(name, date, reason);
+        }
+        void approveOnDutyRequest() {
+            System.out.println("Approving on duty request");
+        }
+        void rejectOnDutyRequest() {
+            System.out.println("Reject on duty request");
+        }
+    }
+    static void main() {
+        LeaveRequest leaveRequest = new LeaveRequest("Sweatha sri R","12-04-2026","High Fever");
+        leaveRequest.approveLeaveRequest();
+        OnDutyRequest onDutyRequest = new OnDutyRequest("Menaga sri R","21-04-2026","Marriage Function");
+        onDutyRequest.rejectOnDutyRequest();
+    }
+}
+
+
+
