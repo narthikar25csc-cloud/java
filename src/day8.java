2feb580ee77c6d114819d1e4feedd2a19e108b03
Narthika Raja <narthikar25csc@vetias.ac.in>
2026-06-15 15:59:47 +0530

    college

--- src/day8.java
+++ src/day8.java
@@ -111,15 +111,15 @@ public class day8 {
     }
     record Random(String reason){
     }
-    static void handleLogin(LoginResult result) {
-        switch (result) {
-            case LoginSuccess success -> System.out.println("Login Success at : " + success.time());
-            case LoginFailure failure -> System.out.println("Login Failure because of " + failure.reason());
+    intln("Login Failure because of " + failure.reason());
             case LoginLoading loading -> System.out.println("Login Loading, " + loading.expectedTime());
-        }
+        }static void handleLogin(LoginResult result) {
+            switch (result) {
+                case LoginSuccess success -> System.out.println("Login Success at : " + success.time());
+                case LoginFailure failure -> System.out.pr
     }
     static void main() {
-        LoginResult LoginSuccess = new LoginSuccess("2026-06-13:03:03:10");
+         LoginResult LoginSuccess = new LoginSuccess("2026-06-13:03:03:10");
         LoginResult LoginFailure = new LoginFailure("Insufficient balance");
         LoginResult LoginLoading = new LoginLoading("Check after 5 hours");
         handleLogin(LoginSuccess);
---
+++ src/day9.java
@@ -0,0 +1,39 @@
+ public class Day9 {
+        static class college implements AutoCloseable {
+            college() {
+                System.out.println("college");
+            }
+
+            @Override
+            public void close() throws Exception {
+                System.out.println("college close");
+            }
+        }
+
+     static class department extends college {
+         department() {
+             System.out.println("department");
+         }
+          @Override
+        public void close() throws Exception {
+            System.out.println("department close");
+            super.close();
+        }
+    }
+    static class year extends department {
+     year() {
+         System.out.println("year");
+     }
+
+        @Override
+        public void close() throws Exception {
+            System.out.println("year Close");
+            super.close();
+        }
+    }
+    static void main() throws Exception {
+        try(year year = new year()){
+
+        }
+    }
+}
