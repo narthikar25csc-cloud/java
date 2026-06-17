4830302e608d618c8b62244bd2b394b1fad0b11c
Narthika Raja <narthikar25csc@vetias.ac.in>
2026-06-17 11:57:18 +0530

    VETIAS

--- src/day10.java
+++ src/day10.java
@@ -40,6 +40,7 @@ public class day10 {
     }
 }
 */
+/*
 import java.util.LinkedList;
 
 public class Day10Linked {
@@ -93,3 +94,37 @@ Student Details;
         }
     }
 }
+ */
+
+      public class day11 {
+        static void main() {
+            String name = null;
+            try {
+                name = null;
+                System.out.println(name.charAt(3));
+                System.out.println(45 / 0);
+            } catch (ArithmeticException e) {
+                System.out.println("Arithmetic Exception");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Array Index of Bound Exception");
+            } catch (Exception e) {
+                System.out.println("Exception");
+            } finally {
+                System.out.println("This will always execute");
+                name="VETIAS";
+            }
+            System.out.println("Still running");
+            System.out.println("Name:" + name);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
