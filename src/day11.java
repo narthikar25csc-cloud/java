5a7109acd90c52d4b85295feebc6ea34e1061d50
Narthika Raja <narthikar25csc@vetias.ac.in>
2026-06-17 14:53:49 +0530

    HashSet

---
+++ src/day11.java
@@ -0,0 +1,19 @@
+
+/*import java.util.HashSet;
+import java.util.Set;
+    public class day11 {
+        public static void main(String[] args) {
+
+           Set<String> set=new HashSet<>();
+           set.add("Student1");
+           set.add("Student2");
+           set.add("student3");
+           set.add("Student1");
+           set.add("Student2");
+           System.out.println("set :");
+             for(String s:set){
+                 System.out.println(s);
+    }
+    }
+}
+ */
