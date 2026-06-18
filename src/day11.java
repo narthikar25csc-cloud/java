859ae1df05be76a0f7bb039a554f447b8eb9dbd3
Narthika Raja <narthikar25csc@vetias.ac.in>
2026-06-18 09:41:39 +0530

    HashMap

--- src/day11.java
+++ src/day11.java
@@ -17,3 +17,20 @@ import java.util.Set;
     }
 }
  */
+/*
+import java.util.HashMap;
+import java.util.Map;
+public class day11 {
+    public static void main(String[]args){
+        Map<String,Integer> person = new HashMap<>();
+        person.put("Ram" , 89);
+        person.put("John" , 78);
+        person.put("Aravindh" , 67);
+        person.put("Ramesh" , 90);
+        System.out.println(person.get("Ram"));
+        person.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
+}
+ */
