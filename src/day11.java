5fbe06add7cf99da839cdfbe66f9bb3a6970df3c
Narthika Raja <narthikar25csc@vetias.ac.in>
2026-06-19 14:40:36 +0530

    Exception

--- src/day11.java
+++ src/day11.java
@@ -16,6 +16,7 @@ import java.util.Set;
     }
     }
 } */
+/*
 import java.util.HashMap;
 import java.util.Map;
 public class day11 {
@@ -31,4 +32,5 @@ public class day11 {
         });
     }
 }
+ */
 
---
+++ src/day13.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+public class Day13 {
+    static void main() {
+        Path path = Path.of("files","report.txt");
+        try{
+            List<String> data = Files.readAllLines(path);
+
+            for (String line : data) {
+                System.out.println(line);
+            }
+            List<String> newData = new ArrayList<>();
+            newData.add("Student1");
+            newData.add("Student2");
+            newData.add("Student3");
+            newData.add("Student4");
+            newData.add("Student5");
+            Files.write(path,newData);
+        } catch (IOException io){
+            io.printStackTrace();
+        }
+    }
+
+}
